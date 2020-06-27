2948caa2491e0e51c063bbe03829647f06fa7843
Gabriel Iasi <31139106+gandradeiasi@users.noreply.github.com>
2020-06-26 22:08:35 -0300

    Formatação de dados

--- assets/js/DiceRoller.js
+++ assets/js/DiceRoller.js
@@ -21,22 +21,26 @@ class DiceRoller {
         const modifier_sign = result.modifier ? (result.modifier < 0 ? '-' : '+') : null;
         result.modifier = modifier_sign == "-" ? result.modifier * -1 : result.modifier;
         const modifier_format = `${result.modifier ? ` ${modifier_sign} ${result.modifier}` : ''}`
-        return `Rolando ${result.results.length}d${result.dice_max}${modifier_format} : ${DiceRoller.formataDices(result)}${modifier_format}${result.results.length > 1 ? ` || Soma = ${result.total}` : ''}`
+        return `Rolando ${result.results.length}d${result.dice_max}${modifier_format} : ${DiceRoller.formataDices(result, " + ")}${modifier_format}${DiceRoller.mostraSufixo(result) ? ` || Soma = ${result.total}` : ''}`
     }
 
     static formataVantagem(result) {
-        return `Rolando ${result.results.length}d${result.dice_max} : ${DiceRoller.formataDices(result)}${result.results.length > 1 ? ` || Maior = ${result.results.reduce((x,y) => y > x ? y : x, 0)}` : ''}`
+        return `Rolando ${result.results.length}d${result.dice_max} : ${DiceRoller.formataDices(result, ", ")}${DiceRoller.mostraSufixo(result) ? ` || Maior = ${result.results.reduce((x,y) => y > x ? y : x, 0)}` : ''}`
     }
 
     static formataDesvantagem(result) {
-        return `Rolando ${result.results.length}d${result.dice_max} : ${DiceRoller.formataDices(result)}${result.results.length > 1 ? ` || Menor = ${result.results.reduce((x,y) => y < x ? y : x, result.results[0])}` : ''}`
+        return `Rolando ${result.results.length}d${result.dice_max} : ${DiceRoller.formataDices(result, ", ")}${DiceRoller.mostraSufixo(result) ? ` || Menor = ${result.results.reduce((x,y) => y < x ? y : x, result.results[0])}` : ''}`
     }
 
-    static formataDices(result) {
+    static mostraSufixo(result) {
+        return result.results.length > 1 || result.modifier;
+    }
+
+    static formataDices(result, separator) {
         let dices = "";
         result.results.forEach((x, n) => {
             dices += x;
-            if (n < result.results.length - 1) dices += ", ";
+            if (n < result.results.length - 1) dices += separator;
         });
         return `[${dices}]`;
     }
